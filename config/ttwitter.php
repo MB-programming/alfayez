71f5572c8361be3d7a7e6556ff6d95e8a0003c91
MB-programming <78989273+MB-programming@users.noreply.github.com>
2026-03-28 08:44:44 +0100

    Add files via upload

---
+++ config/ttwitter.php
@@ -0,0 +1,21 @@
+<?php
+
+// You can find the keys here : https://apps.twitter.com/
+
+return [
+	'debug'               => false,
+
+	'API_URL'             => 'api.twitter.com',
+	'UPLOAD_URL'          => 'upload.twitter.com',
+	'API_VERSION'         => '1.1',
+	'AUTHENTICATE_URL'    => 'https://api.twitter.com/oauth/authenticate',
+	'AUTHORIZE_URL'       => 'https://api.twitter.com/oauth/authorize',
+	'ACCESS_TOKEN_URL'    => 'https://api.twitter.com/oauth/access_token',
+	'REQUEST_TOKEN_URL'   => 'https://api.twitter.com/oauth/request_token',
+	'USE_SSL'             => true,
+
+	'CONSUMER_KEY'        => 'sulaV5ePidNPOMAX7MWkI2wwd',
+	'CONSUMER_SECRET'     => 'HOOAQC6zXuKCrAywPxiMXaybgSgxi9fR2Jgm5VvDbwpgtmh24i',
+	'ACCESS_TOKEN'        => '412288089-klWgSDIVoJc6YK57ijz8hFQzrjtgWfShVZzKoF2q',
+	'ACCESS_TOKEN_SECRET' => '4YlihbYsWH5RN8Yv7weZZxRsubk7i2MrmOmilxegHvdYK',
+];
---
+++ config/youtube.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+// You can find the keys here : https://console.developers.google.com
+
+return array(
+    // 'KEY' => 'AIzaSyBTFFUKTcdCE0fCyOLB2Cno5zXpPmUisOo'
+    'KEY' => 'AIzaSyCD0_WSExapMyfqDu9u7ZWPXieicgHHN3A'
+);
